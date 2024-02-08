41617f8c941ce81042cbf780d07f30a32af09604
Mendis, Steve Collin <smendis@iu.edu>
2024-02-08 15:29:10 -0500

    Initial commit - index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+
+/*
+Create a username and email address for a student.
+ * @param { string } fullName
+ * @return { array } userName and email
+*/
+function createEmail(fullName) {}
+
+let promptName = prompt('Enter your full name:');
+let userInfo = createEmail(promptName);
